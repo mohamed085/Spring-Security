790737ecc645eeb37aa2256e92d0887d2043fa74
mohamed085 <mohamed085555@gmail.com>
2021-08-29 22:30:08 +0200

    Create Spring Initializer class

--- src/main/java/com/spring/config/SpringMVCDispatcherServletInitializer.java
+++ src/main/java/com/spring/config/SpringMVCDispatcherServletInitializer.java
@@ -1,5 +1,22 @@
 package com.spring.config;
 
 
-public class SpringMVCDispatcherServletInitializer  {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
 }
